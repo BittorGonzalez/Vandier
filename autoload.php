a2ccf7bf455b1b23691aa824be58bb46657cffba
BittorGonzalez <txort2@gmail.com>
2023-11-18 18:48:22 +0100

    Autoload implementado para automatizar la carga de clases

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+//INFO: Carga de clases automaticamente en cualquier archivo del proyecto
+spl_autoload_register(function ($clase) {
+
+    $archivo = __DIR__ ."/".  $clase . ".php";
+    $archivo = str_replace("\\","/", $archivo);
+
+    if(is_file($archivo)) {
+        require $archivo;
+    }
+});
+
+
+?>
