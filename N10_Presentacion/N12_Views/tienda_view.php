76d546706e23c885604a1c3a260fa28310712fa6
BittorGonzalez <txort2@gmail.com>
2023-11-24 23:57:27 +0100

    Estructura principal de la pagina de tienda

--- N10_Presentacion/N12_Views/tienda_view.php
+++ N10_Presentacion/N12_Views/tienda_view.php
@@ -1,41 +1,143 @@
 <main>
-    <section class="hero container-fluid d-flex flex-column justify-content-center align-items-center">
+    <section class="hero vh-100 container-lg d-flex flex-column justify-content-center align-items-center">
         <h2 class="mainTitle text-center fw-bold">CATALOGO DE PRODUCTOS</h2>
-        <i class="fa-solid fa-chevron-down fs-1 mt-3"></i>
+        <i class="fa-solid fa-chevron-down fs-1 mt-5"></i>
     </section>
 
 
-    <section class="categorias_bar container-fluid d-flex flex-column justify-content-center align-items-center">
+    <section class="gridProductos container-lg d-flex flex-column justify-content-center align-items-center">
 
-            <nav>
-                <ul class="d-flex list-unstyled gap-5">
+        <nav class="categoriasBarra d-flex align-items-center gap-4">
 
-                    <li class="border border-white py-2 px-4"><a class="text-decoration-none text-white" href="">MANO</a></li>
-                    <li class="border border-white py-2 px-4"><a  class="text-decoration-none text-white" href="">CABEZA</a></li>
-                    <li class="border border-white py-2 px-4"><a  class="text-decoration-none text-white" href="">CUELLO</a></li>
-                    <li class="border border-white py-2 px-4"><a class="text-decoration-none text-white" href="">ROPA</a></li>
-                    <li class="border border-white py-2 px-4"><a class="text-decoration-none text-white" href="">OTROS</a></li>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">TODOS</button>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">MANOS</button>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">CABEZA</button>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">CUELLO</button>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">ROPA</button>
+            <button type="button"
+                class="btnCategoria btn border border-white px-4 py-2 text-white fw-bolder">OTROS</button>
 
-                </ul>
-            </nav>
+        </nav>
 
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            
+
+        <div class="productos container-lg gap-5">
+
+            <div class="col">
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+
+                <div class="card w-100 border-0 rounded-3">
+                    <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                            class="img img-responsive w-100">
+                        <div class="profile-name">OMNITRIX</div>
+                        <div class="profile-username">2€</div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
 
     </section>
 </main>
