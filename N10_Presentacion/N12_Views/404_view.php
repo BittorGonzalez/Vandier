e65d05d82b7b5cd94b4ca4849db97ae2570fcac2
Jul3n0405 <julenpe515@gmail.com>
2023-11-24 14:51:50 +0100

    inicio continuado, cards creadas, scss añadidos, z-index del navbar ajustado

--- N10_Presentacion/N12_Views/404_view.php
+++ N10_Presentacion/N12_Views/404_view.php
@@ -1,7 +1,4 @@
-
-
 <main>
-
     <img src="N10_Presentacion\N14_Assets\Images\404.png" alt="">
 </main>
 
--- N10_Presentacion/N12_Views/inicio_view.php
+++ N10_Presentacion/N12_Views/inicio_view.php
@@ -1,4 +1,30 @@
 <main>
-    <h2>PAGINA DE INICIO</h2>
-    <img src="" alt="">
+    <section class="fotoinicio">
+        <img class="imagenprincipal" src="N10_Presentacion\N14_Assets\Images\fotoprincipal.jpg" alt="">
+    </section>
+
+    <section class="cards">
+        <div class="card">
+            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\reloj_1.webp"
+                    class="img img-responsive">
+                <div class="profile-name">OMNITRIX</div>
+                <div class="profile-username">2€</div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
+                    class="img img-responsive">
+                <div class="profile-name">OMNITRIX</div>
+                <div class="profile-username">2€</div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\cartera_1.webp"
+                    class="img img-responsive">
+                <div class="profile-name">OMNITRIX</div>
+                <div class="profile-username">2€</div>
+            </div>
+        </div>
+    </section>
+    
 </main>
