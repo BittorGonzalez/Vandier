8f6587a4c37c7a05d239873036420d0b0f733d19
gaizka9 <g1995788@gmail.com>
2023-11-24 14:51:23 +0100

    login

--- N10_Presentacion/N12_Views/login_view.php
+++ N10_Presentacion/N12_Views/login_view.php
@@ -1,1 +1,91 @@
-<h2>LOGIN</h2>
+<section class="vh-100 gradient-custom" id="Signin">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+
+              <div class="mb-md-5 mt-md-4 pb-5">
+
+                <h2 class="fw-bold mb-2 text-uppercase">Registrate</h2>
+                <p class="text-white-50 mb-5">Por favor introduzca sus datos!</p>
+
+                <div class="form-outline form-white mb-4">
+                  <input type="email" placeholder="Email" id="typeEmailX" class="form-control form-control-lg" />
+                </div>
+
+                <div class="form-outline form-white mb-4">
+                  <input type="password" placeholder="Contraseña" id="typePasswordX"
+                    class="form-control form-control-lg" />
+                </div>
+
+
+                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+              </div>
+
+              <div>
+                <p class="mb-0">No tienes una cuenta? <a href="#!" class="text-white-50 fw-bold" id="Sup">Sign Up</a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="vh-100 gradient-custom" id="Signup">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+
+              <div class="mb-md-5 mt-md-4 pb-5">
+
+                <h2 class="fw-bold mb-2 text-uppercase">Crea una cuenta</h2>
+                <p class="text-white-50 mb-5">Por favor introduzca sus datos!</p>
+
+
+                <div class="form-outline form-white  mb-4">
+                  <input type="text" placeholder="Nombre" id="typeName" class="form-control form-control-lg" />
+                </div>
+
+                <div class="form-outline form-white mb-4">
+                  <input type="text" placeholder="Apellido" id="typeLastname" class="form-control form-control-lg" />
+                </div>
+
+
+                <div class="form-outline form-white mb-4">
+                  <input type="email" placeholder="Email" id="typeEmail" class="form-control form-control-lg" />
+                </div>
+
+                <br><br>
+
+                <div class="form-outline form-white mb-4">
+                  <input type="password" placeholder="Contraseña" id="typePassword1"
+                    class="form-control form-control-lg" />
+                </div>
+
+                <div class="form-outline form-white mb-4">
+                  <input type="password" placeholder="Contraseña" id="typePassword2"
+                    class="form-control form-control-lg" />
+                </div>
+
+                <button class="btn btn-outline-light btn-lg px-5" type="submit">Sign up</button>
+              </div>
+
+              <div>
+                <p class="mb-0">Ya tienes una cuenta? <a href="#!" class="text-white-50 fw-bold" id="Sin">Sign In</a>
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
