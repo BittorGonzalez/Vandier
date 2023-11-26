6f752e9dda4d79180f3c37eb49a09ac3df58a0d7
DESKTOP-5VL82C7\julen <j_perez@fpzornotza.com>
2023-11-26 17:27:27 +0100

    Cards con responsive de bootstrap hechas. (CARDS TERMINADAS)

--- N10_Presentacion/N12_Views/inicio_view.php
+++ N10_Presentacion/N12_Views/inicio_view.php
@@ -3,28 +3,40 @@
         <img class="imagenprincipal" src="N10_Presentacion\N14_Assets\Images\fotoprincipal.jpg" alt="">
     </section>
 
-    <section class="cards">
-        <div class="card">
-            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\reloj_1.webp"
-                    class="img img-responsive">
-                <div class="profile-name">OMNITRIX</div>
-                <div class="profile-username">2€</div>
+
+
+    <div class="container m-4">
+        <section class="row row-cols-xl-4 row-cols-md-2 row-cols-1">
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="profile-card-2 m-2">
+                    <img src="N10_Presentacion\N14_Assets\Images\reloj_1.webp" class="img img-responsive">
+                    <div class="profile-name">Vandier Titanium</div>
+                    <div class="profile-username">334,99€</div>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp"
-                    class="img img-responsive">
-                <div class="profile-name">OMNITRIX</div>
-                <div class="profile-username">2€</div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="profile-card-2 m-2">
+                    <img src="N10_Presentacion\N14_Assets\Images\gafas_1.webp" class="img img-responsive">
+                    <div class="profile-name"></div>
+                    <div class="profile-username"></div>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="profile-card-2"><img src="N10_Presentacion\N14_Assets\Images\cartera_1.webp"
-                    class="img img-responsive">
-                <div class="profile-name">OMNITRIX</div>
-                <div class="profile-username">2€</div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="profile-card-2 m-2">
+                    <img src="N10_Presentacion\N14_Assets\Images\cartera_1.webp" class="img img-responsive">
+                    <div class="profile-name"></div>
+                    <div class="profile-username"></div>
+                </div>
             </div>
-        </div>
-    </section>
-    
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="profile-card-2 m-2">
+                    <img src="N10_Presentacion\N14_Assets\Images\cartera_1.webp" class="img img-responsive">
+                    <div class="profile-name"></div>
+                    <div class="profile-username"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
 </main>
