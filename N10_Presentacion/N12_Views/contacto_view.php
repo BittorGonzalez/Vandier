633404d27c8cc1a7547619336a6641a1dcd5a777
osdomz <osdom.dev@hotmail.com>
2023-11-27 09:17:21 +0100

    Revert "update contacto"
    This reverts commit 341f4f475488d67d5f4d3d726c8abc6d7151f0d5.

--- N10_Presentacion/N12_Views/contacto_view.php
+++ N10_Presentacion/N12_Views/contacto_view.php
@@ -1,50 +1,6 @@
 <main>
 
-    <div class="bg-background"></div>
-    <div class="container py-5">
-        <div class="row py-5 g-3">
+    <h2>PAGINA DE CONTACTO</h2>
 
-            <div class="col-md-6 first_col ">
-                <h1 class="text-center mt-3">CONTACT US</h1>
-                <form class="p-4 mt-5">
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Enter your Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email ID</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">Enter your massage</label>
-                        <textarea  type="text" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <button class="btn btn-primary">Send Now</button>
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-6 sec_col">
-                <img src="N10_Presentacion\N14_Assets\Images\deco_3.webp"
-                    class="img-fluid">
-            </div>
-        </div>
-        <div class="row-last">
-            <div class="row row-cols-1 row-cols-md-3  p-3 text-white">
-                <div class="col">
-                    <h4>CALL US</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, doloribus!</p>
-                </div>
-                <div class="col">
-                    <h4>LOCATION</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, doloribus!</p>
-                </div>
-                <div class="col">
-                    <h4>EMAIL</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, doloribus!</p>
-                </div>
-            </div>
-        </div>
-    </div>
 
 </main>
